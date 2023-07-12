e3a2867f21d5760fb0dfd13b4550639fbeaeaddd
torobi <torobi2222@gmail.com>
2023-07-13 02:40:50 +0900

    config修正

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_PAGES ? "reactailwind" : "./",
+  base: process.env.GITHUB_PAGES ? "gridtimer" : "./",
   plugins: [react()],
 });
